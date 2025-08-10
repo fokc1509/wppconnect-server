6b61cae25d6646053b051195a92adff64e55d35f
fokc1509 <felipechukr@gmail.com>
2025-08-10 20:20:46 -0300

    Update config.ts
    ajuste de token padrao

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { ServerOptions } from './types/ServerOptions';
 
 export default {
-  secretKey: 'THISISMYSECURETOKEN',
+  secretKey: '546sf5d4f6ds4f65f4s6fw',
   host: 'http://localhost',
   port: '21465',
   deviceName: 'WppConnect',
